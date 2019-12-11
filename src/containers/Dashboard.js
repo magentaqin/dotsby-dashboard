600a1ed873f0b2fc5144a8dccf6e35460605d7b6
Mu Qin <magenta2127@gmail.com>
2019-12-11 22:36:09 +0800

    add layout for dashboard

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Icon, Button } from 'antd';
 
 const Dashboard = () => {
   return (
-    <h1>Hello, I am Dashboard.</h1>
+    <div className="dashboard-container">
+      <div className="top-bar">
+        <div className="top-inner-wrapper">
+          <h3>Dotsby Dashboard</h3>
+          <div>
+            <p>hello@gmail.com</p>
+          </div>
+        </div>
+      </div>
+      <div className="add-button-container">
+        <div className="add-button-inner-wrapper">
+          <Button type="primary">
+            <Icon type="plus" />
+            Create New Document
+          </Button>
+        </div>
+      </div>
+      <ul>
+        <li>Docs List</li>
+      </ul>
+    </div>
   )
 }
 
